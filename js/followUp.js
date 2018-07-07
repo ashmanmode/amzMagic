f69d4f77d42ac70c5dde7c8e152f71c62bded596
Ashish Manmode <ash.manmode@gmail.com>
2018-07-07 17:35:40 +0530

    single upload frontend

---
+++ js/followUp.js
@@ -0,0 +1,45 @@
+function httpGetAsync(theUrl, callback)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+    }
+    xmlHttp.open("GET", theUrl, true); // true for asynchronous 
+    xmlHttp.send(null);
+}
+
+function singleFollowUp()
+{
+	var order_id = document.getElementById("order_id").value;
+	var buyer_name = document.getElementById("buyer_name").value;
+	var buyer_email = document.getElementById("buyer_email").value;
+	var product_sku = document.getElementById("product_sku").value;
+	var tracking_number = document.getElementById("tracking_number").value;
+	var email_template_id = document.getElementById("email_template_id").value;
+
+	// Create JSON
+	var json = ''
+	json += '{';
+	json += '"order_id":"'+order_id+'",';
+	json += '"buyer_name":"'+buyer_name+'",';
+	json += '"buyer_email":"'+buyer_email+'",';
+	json += '"product_sku":"'+product_sku+'",';
+	json += '"tracking_number":"'+tracking_number+'",';
+	json += '"email_template_id":"'+email_template_id+'"';
+	json += '} ';
+
+	var url = 'https://script.google.com/macros/s/AKfycbyKkqSxd9OTBvG6xP_1PCrTeAnVFj7XN9CoJruABe1D5UFjZLEn/exec?'
+
+	var send_url = url+'data=' + json;
+	
+	console.log(send_url); 
+
+	$.get(send_url, function(data, status){
+        alert("Data: " + data + "\nStatus: " + status);
+    });
+	
+}
+
+var instance = M.Tabs.init(document.getElementById("tabs_ashish"));
+
